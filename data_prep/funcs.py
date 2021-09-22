1311f7aced323bae501421f9dc090224e1280b2b
SamuelBaptista <samuelbaptista27@gmail.com>
2021-09-22 12:03:32 -0300

    processed form data v1

--- data_prep/funcs.py
+++ data_prep/funcs.py
@@ -1,4 +1,5 @@
 from unidecode import unidecode
+import numpy as np
 
 def clean_column_names(columns):
     clean_columns = []
@@ -14,3 +15,8 @@ def clean_column_names(columns):
         )
     
     return clean_columns
+
+
+def euclidean_distance(a, b):
+    """Return the array of distances of two numpy arrays of points."""
+    return np.linalg.norm(a - b, axis=1)
