72042ecca246598d6ccd0e7613a4c294b5d44cdb
SamuelBaptista <samuelbaptista27@gmail.com>
2021-09-23 16:42:34 -0300

    first reports

--- src/io_helper.py
+++ src/io_helper.py
@@ -36,7 +36,7 @@ def read_tsp(filename):
                 node_coord_start = i
             i = i+1
 
-        print('Problem with {} cities read.'.format(dimension))
+        print('Problem with {} crops read.'.format(dimension))
 
         f.seek(0)
 
